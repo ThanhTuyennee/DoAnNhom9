5d7fd266edf70aa1ea5a0da2ae2eeaad4d77d71f
Thanh Tuyền <ltthahtyn945@gmail.com>
2025-10-31 15:36:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,6 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Trang bán hàng</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                         <a href="banhang.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i>Tạo đơn hàng</a>
                     </div>
